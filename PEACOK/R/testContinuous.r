55713b6131489186e9894635ad15215723a7faba
Javier Sanchez Lorente <67375457+javiersanchezlorente@users.noreply.github.com>
2024-09-12 15:24:43 +0200

    Update testContinuous.r

--- PEACOK/R/testContinuous.r
+++ PEACOK/R/testContinuous.r
@@ -21,6 +21,7 @@
 test.continuous <- function(opt, vl, varName, varType, thisdata, phenoStartIdx) {
   	cat("CONTINUOUS MAIN || ")	
   	pheno <- thisdata[,phenoStartIdx:ncol(thisdata)]
+	pheno <- as.data.frame(lapply(pheno, function(x) as.numeric(as.character(x))))
   	# reassign values
   	thisdata[,phenoStartIdx:ncol(thisdata)] <-  .reassignValue(vl, pheno, varName)
   	.test.continuous2(opt, vl, varName, varType, thisdata, phenoStartIdx)
@@ -31,6 +32,7 @@ test.continuous <- function(opt, vl, varName, varType, thisdata, phenoStartIdx) 
 .test.continuous2 <- function(opt, vl, varName, varType, thisdata, phenoStartIdx) {
 	  cat("CONTINUOUS || ")
 	  pheno <- thisdata[,phenoStartIdx:ncol(thisdata)]
+	  pheno <- as.data.frame(lapply(pheno, function(x) as.numeric(as.character(x))))
 	  isExposure <- get.is.exposure(vl, varName)
 
   	if (!is.null(dim(pheno))) {
